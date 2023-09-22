43050b3a7a893b23cd4a2b9a97f469e585384541
GmoZen <zendejas.guillermo@gmail.com>
2023-09-22 03:52:38 -0700

    Add gifts json data and gifts route

---
+++ server/data/gifts.js
@@ -0,0 +1,96 @@
+
+
+const giftData = [
+    {
+        "id": 1,
+        "name": "Disco Ball Candle",
+        "pricePoint": "$",
+        "audience": "Candle Lovers",
+        "image":"https://m.media-amazon.com/images/I/81q1IjzVu-L.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+        "description":"A party in a candle",
+        "submittedBy":"Sasha",
+        "submittedOn":"2022-09-04T14:48:00"
+    },
+    {
+        "id": 2,
+        "name": "Lego Flower Bouquet Kit",
+        "pricePoint": "$",
+        "audience": "Green Thumbs",
+        "image":"https://m.media-amazon.com/images/I/71y9it7UJkL._AC_UL640_FMwebp_QL65_.jpg",
+        "description":"A gift and fun activity to do together",
+        "submittedBy":"Isabel",
+        "submittedOn":"2023-02-14T12:32:00"
+    },
+    {
+        "id": 3,
+        "name": "Nintendo Switch Lite",
+        "pricePoint": "$$",
+        "audience": "Gamers",
+        "image":"https://m.media-amazon.com/images/I/61X2tTrtDSL._AC_UY436_FMwebp_QL65_.jpg",
+        "description":"A gift for gamers on-the-go",
+        "submittedBy":"Mario",
+        "submittedOn":"2022-09-13T11:01:00"
+    },
+    {
+        "id": 4,
+        "name": "Nike Panda Dunks",
+        "pricePoint": "$$",
+        "audience": "Sneakerheads",
+        "image":"https://m.media-amazon.com/images/I/61rLMYGKrNL._AC_UL640_FMwebp_QL65_.jpg",
+        "description":"A fan-favorite black and white color-blocking sneaker",
+        "submittedBy":"Jason",
+        "submittedOn":"2023-03-10T05:23:00"
+    },
+    {
+        "id": 5,
+        "name": "Razer Kitty Headset",
+        "pricePoint": "$$",
+        "audience": "Gamers",
+        "image":"https://m.media-amazon.com/images/I/616y3R63PcL._AC_UY436_FMwebp_QL65_.jpg",
+        "description":"Create the purrfect gaming look with your own killer kitty style",
+        "submittedBy":"Pixie",
+        "submittedOn":"2011-10-10T14:48:00"
+    },
+    {
+        "id": 6,
+        "name": "Nellsi Sunset Lamp Projection",
+        "pricePoint": "$",
+        "audience": "Sungazers",
+        "image":"https://m.media-amazon.com/images/I/716jpzJZlaL._AC_UL640_FMwebp_QL65_.jpg",
+        "description":"Sunshine any time, any place",
+        "submittedBy":"Melody",
+        "submittedOn":"2011-10-10T14:48:00"
+    },
+    {
+        "id": 7,
+        "name": "Frigidaire Retro Mini Fridge",
+        "pricePoint": "$",
+        "audience": "Foodies",
+        "image":"https://m.media-amazon.com/images/I/61DB93l5wlL._AC_UY436_FMwebp_QL65_.jpg",
+        "description":"A retro way of keeping things cool",
+        "submittedBy":"Grover",
+        "submittedOn":"2023-01-01T14:48:00"
+    },
+    {
+        "id": 8,
+        "name": "Crosley Discover Turntable",
+        "pricePoint": "$",
+        "audience": "Music Lovers",
+        "image":"https://m.media-amazon.com/images/I/812yx0TJnhL._AC_UY436_FMwebp_QL65_.jpg",
+        "description":"A portable turntable that is ready to play right out of the box",
+        "submittedBy":"Marvin",
+        "submittedOn":"2023-04-01T07:02:00"
+    },
+    {
+        "id": 9,
+        "name": "Brandon Blackwood Parker Bag",
+        "pricePoint": "$$",
+        "audience": "Fashionistas",
+        "image":"https://m.media-amazon.com/images/I/61+rRvMlhIL._AC_UL640_FMwebp_QL65_.jpg",
+        "description":"A perfect pop of color",
+        "submittedBy":"Brandon",
+        "submittedOn":"2023-01-10T14:48:00"
+    }
+]
+
+export default giftData
