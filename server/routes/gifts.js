903557c6d5a4f3f11c900aad866a5e42f9485f06
GmoZen <zendejas.guillermo@gmail.com>
2023-09-22 18:12:52 -0700

    Add detail data to detail html elements dynamically

--- server/routes/gifts.js
+++ server/routes/gifts.js
@@ -12,5 +12,9 @@ router.get('/', (req, res) => {
     res.status(200).json(giftData)
 })
 
+router.get('/:giftId', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, '../public/gift.html'))
+})
+
 
 export default router
