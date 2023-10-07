3d418838a83a6e746d676fffe89901b9f548ddb0
GmoZen <zendejas.guillermo@gmail.com>
2023-10-06 23:47:26 -0700

    Write a function in controller to fetch all gifts from the server

--- server/controllers/gifts.js
+++ server/controllers/gifts.js
@@ -1,17 +1,33 @@
 import { pool } from '../config/database.js'
 
 const getGifts = async (req, res) => {
-
     try {
         const results = await pool.query('SELECT * FROM gifts ORDER BY id ASC')
-
         res.status(200).json(results.rows)
-
     } catch (error) {
         res.status(409).json( { error: error.message } )
     }
 }
 
+const getGiftById = async (req, res) => {
+    try {
+        const giftId = req.params.giftId
+        selectQuery = `SELECT name, pricePoint, audience, image, description, submittedBy, submittedOn FROM gifts WHERE id = $1`
+        const results = await pool.query(selectQuery, [giftId])
+
+        res.status(200).json(results.rows[0])
+
+        // console.log(res)
+
+    } catch (error) {
+        res.status(409).json( { error: error.message })
+    }
+}
+
+
+
+
 export default { 
-    getGifts 
+    getGifts,
+    getGiftById 
 }
